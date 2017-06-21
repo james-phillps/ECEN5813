b6e7927a04aa2b4a82e81c6737a2b259962660e9
James Phillips <james.phillips-1@colorado.edu>
2017-06-20 20:55:25 -0600

    Adding Jordan's debug.c to master

---
+++ project1/debug.c
@@ -0,0 +1,19 @@
+
+// #include
+
+main()
+{
+
+/*!
+* @enable/disable debug printing with compile time switch
+* if enabled, this takes a pointer to memory and prints the bytes given to a pointer location and the length of the bytes to print
+* @Jordan Wachs
+* @06-18-17
+*/
+
+void print_memory(uint8_t * start, uint32_t length)
+
+
+
+
+}
