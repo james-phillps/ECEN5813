7c42b8bc6a13b63eaad864297ad09750ff44b20c
jo-wa <jowa8177@colorado.edu>
2017-06-21 16:17:44 -0600

    Adding debug.h

---
+++ project1/debug.h
@@ -0,0 +1,10 @@
+/**
+ * @file debug.h
+ * @brief Function prototypes for debug.c
+ * Contains function prototypes for debug function
+ * @author Jordan Wachs
+ * @date 6/21/2017
+ */
+
+
+void print_memory(uint8_t * start, uint32_t length);
