f68886c26707d14bdee3af93c16feccbf9fb0904
James Phillips <james.phillips-1@colorado.edu>
2017-06-22 19:54:03 -0600

    Implemented little_to_big32 and removed main function from conversion.c

--- project1/conversion.c
+++ project1/conversion.c
@@ -1,12 +1,11 @@
+/**
+ * @file conversion.c
+ * @brief Data conversion routines
+ * Routines to convert data types and endianness
+ * @author James Phillips and Jordan Wachs
+ * @date 6/18/2017
+ */
 
-#include <stdio.h>
-#include <stdint.h>
-//#include "conversion.h"
-//#include "project1.h"
-
-
-void main()
-{
 /*!
 * @integer to ASCII converter
 * Converts a standard (signed) integer type to ASCII string and saves to a memory location specified with an input pointer.  Can support bases 2 to 16 integer inputs.  String ends with a null terminate.
@@ -24,8 +23,8 @@ uint8_t my_itoa(int32_t data, uint8_t * ptr, uint32_t base) {
 /*!
 * @ASCII to integer converter
 * Converts ASCII character set to int32_t data.  Signed values can be input and bases 2 to 16 are supported
-* @Jordan Wachs
-* @06-18-17
+* @James Phillips
+* @06-22-17
 */
 
 int32_t my_atoi(uint8_t * ptr, uint8_t digits, uint32_t base) {
@@ -89,18 +88,22 @@ int8_t big_to_little32(uint32_t * data, uint32_t length) {
 /*!
 * @Little endian to Big Endian converter
 * Converts array from little endian to big endian representation.  Returns error if conversion fails
-* @Jordan Wachs
+* @James Phillips
 * @06-18-17
 */
 
 int8_t little_to_big32(uint32_t * data, uint32_t length) {
 
-	const char * ErrFunc() {
-	const char *ErrFlag = "Error in little_to_big32";
-	return ErrFlag;
-	}
+	uint32_t temp = 0;
 
-}
+  for (int i = 0; i < (length/2); i++)
+  {
+    temp = *(data + i);
+    *(data + i) = *(data + length - 1 - i);
+    *(data + length - 1 - i) = temp;
+  }
+
+	return 0;
 
 
 }
