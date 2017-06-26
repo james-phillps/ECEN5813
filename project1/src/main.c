49016ff1e1ad84ac71f94933d3556fc0311a2c54
James Phillips <james.phillips-1@colorado.edu>
2017-06-25 21:10:52 -0600

    Added main.c

---
+++ project1/src/main.c
@@ -0,0 +1,15 @@
+#include "../include/common/conversion.h"
+#include "../include/common/debug.h"
+#include "../include/common/memory.h"
+#include "../include/common/project1.h"
+
+#include <stdio.h>
+#include <stdint.h>
+
+int main(void)
+{
+  #ifdef PROJECT1
+  course1();
+  #endif
+  return 0;
+}
